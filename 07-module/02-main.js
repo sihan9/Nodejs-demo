feaf41e9355a76b5fb5c04f9f5991b6d72299e16
sihan9 <1060192071@qq.com>
2019-09-10 17:09:52 +0800

    add 07

---
+++ 07-module/02-main.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+const p = require('./02-export-var.js');
+
+console.dir(module);
+console.log(p);*/
+
+/*
+const circle = require('./02-export-function.js');
+console.log('r = 10,circle area:%d',circle(10).area());
+console.log('r = 10,circle circumference:%d',circle(10).circumference());
+console.dir(module);*/
+
+/*
+const circle =  require('./02-export-object.js'),
+      log = console.log;
+log('r = 10 ,circle diameter:',circle.diameter(10));
+log('r = 10 ,circle area:',circle.area(10));*/
+const circle = require('./02-export-object-v2.js'),
+  log = console.log;
+log('r = 10 ,circle area:',circle.area(10));
+log('r = 10 ,circle area:',circle.area(10));
