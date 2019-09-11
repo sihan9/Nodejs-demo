8e3c443ff5dfddabe06ea8f3d577b368c6b394a1
sihan9 <1060192071@qq.com>
2019-09-11 17:16:33 +0800

    add 08

---
+++ 08-event/03-listen-radio.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Radio = require('./03-radio.js');
+
+const station = {
+  freq:'106.7',
+  name:'music radio'
+};
+
+var radio = new Radio(station);
+
+radio.on('play',(station)=>{
+  console.log('"%s" FM %s opened!',station.name,station.freq);
+});
+radio.on('stop',(station)=>{
+  console.log('"%s" FM %s closed!',station.name,station.freq);
+});
