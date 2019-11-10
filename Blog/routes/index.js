992fb539fc1157c3ef4de1bbdfd30f7fb79b86d9
sihan9 <1060192071@qq.com>
2019-11-10 19:03:47 +0800

    add blog

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var data = require('./data.json');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use(express.static(__dirname));//显示图片
+
+router.get('/data', function(req, res) {
+  res.send(data);
 });
-
+router.get('/login', function(req, res) {
+  res.type('text/html');
+  res.status(200);
+  res.sendFile(`${__dirname}/login.html`);
+});
+router.get('/list', function(req, res) {
+  for(var i =0 ;i<data.users.length ;i++){
+    let username = data.users[i].username;
+    let pwd = data.users[i].password;
+    if(req.query.username === username && req.query.pwd === pwd){
+    //   res.end(()=>{
+        res.type('text/html');
+        res.status(200);
+        res.sendFile(`${__dirname}/list.html`);
+    //   });
+      break;
+    }
+    res.send('用户名或密码错误！');
+  }
+});
 module.exports = router;
